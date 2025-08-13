06b37cf9682de427e296c51a8e2223f42bd9b5cf
star14ms <68639278+star14ms@users.noreply.github.com>
2025-08-13 09:03:52 +0900

    Feat: Constellation Achievements

--- crawling/filename_parser.py
+++ crawling/filename_parser.py
@@ -4,26 +4,26 @@ import shutil
 from pathlib import Path
 
 
-base_folder = "src\shared\constellations_with_props"
+base_folder = "src\shared\constellations"
 
 for root, _, files in os.walk(base_folder):
     for filename in files:
         # Rename
-        # if filename.endswith(".csv"):
-        #     # Match filenames with _{i}.csv pattern
-        #     match = re.match(r"^(.*)_\d+\.csv$", filename)
-        #     if match:
-        #         new_name = f"{match.group(1)}.luau"
-        #         old_path = os.path.join(root, filename)
-        #         new_path = os.path.join(root, new_name)
-                
-        #         # Avoid overwriting if multiple files collapse into one name
-        #         if os.path.exists(new_path):
-        #             print(f"Skipping {old_path} → {new_path} (already exists)")
-        #         else:
-        #             os.rename(old_path, new_path)
-        #             print(f"Renamed: {old_path} → {new_path}")
-        
+        if filename.endswith(".csv"):
+            # Match filenames with _{i}.csv pattern
+            # match = re.match(r"^(.*)_\d+\.csv$", filename)
+            new_name = f"data.csv"
+            # new_name = f"{match.group(1)}.luau"
+            old_path = os.path.join(root, filename)
+            new_path = os.path.join(root, new_name)
+            
+            # Avoid overwriting if multiple files collapse into one name
+            if os.path.exists(new_path):
+                print(f"Skipping {old_path} → {new_path} (already exists)")
+            else:
+                os.rename(old_path, new_path)
+                print(f"Renamed: {old_path} → {new_path}")
+    
         # # Copy
         # if filename.endswith(".csv"):
         #     name_no_ext = Path(filename).stem  
@@ -38,13 +38,13 @@ for root, _, files in os.walk(base_folder):
         #         shutil.copyfile(old_path, new_path)
         #         print(f"Copied: {old_path} → {new_path}")
 
-        # Remove
-        if filename.endswith(".luau"):
-            path = os.path.join(root, filename)
+        # # Remove
+        # if filename.endswith(".luau"):
+        #     path = os.path.join(root, filename)
             
-            # Avoid overwriting if multiple files collapse into one name
-            if not os.path.exists(path):
-                print(f"Skipping {path} (no exists)")
-            else:
-                os.remove(path)
-                print(f"Removed: {path}")
+        #     # Avoid overwriting if multiple files collapse into one name
+        #     if not os.path.exists(path):
+        #         print(f"Skipping {path} (no exists)")
+        #     else:
+        #         os.remove(path)
+        #         print(f"Removed: {path}")
